Included normalization and full-scale run. Fixed epoch counter

HERMES/models/blocks.py
@@ -30,7 +30,7 @@ class convBlock(nn.Module):
         """
         super(convBlock, self).__init__()
         self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
-        self.bn = nn.BatchNorm1d(out_channels)
+        # self.bn = nn.SyncBatchNorm(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.final_layer = final_layer
         self.dropout = nn.Dropout(p=0.5)
@@ -43,9 +43,9 @@ class convBlock(nn.Module):
             x (Tensor): Input tensor.
         """
         inputs = x # 64
-        x = F.interpolate(x, scale_factor=2) # 128
+        # x = F.interpolate(x, scale_factor=2) # 128
         x = self.conv(x) # Calculate this (x,)
-        x = self.bn(x)
+        # x = self.bn(x)
         if not self.final_layer:
             x = self.dropout(x)
         x = self.relu(x)

HERMES/models/dataset.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from torch.utils.data import Dataset
 import numpy as np
 import pandas as pd
+import torch
 
 # Create a custom dataset class that inherits from DatasetFolder
 
@@ -10,10 +11,22 @@ class ToTensor(object):
     """Convert ndarrays in sample to Tensors."""
 
     def __call__(self, sample):
-        spectra, parameters = sample['spectra'], sample['parameters']
+        spectra, parameters, rtop = sample['spectra'], sample['parameters'], sample['rtop']
 
-        return {'spectra': torch.from_numpy(image),
-                'parameters': torch.from_numpy(landmarks)}
+        return {'spectra': torch.from_numpy(spectra),
+                'parameters': torch.from_numpy(parameters),
+                'rtop': torch.from_numpy(rtop)}
+
+class Normalize(object):
+
+    def __call__(self, sample, normalization_factor=1e-3):
+        spectra, parameters, rtop = sample['spectra'], sample['parameters'], sample['rtop']
+
+        spectra = spectra / normalization_factor
+
+        return {'spectra': spectra,
+                'parameters': parameters,
+                'rtop': rtop}
 
 class spectraDataset(Dataset):
     def __init__(self, csv_file, root_dir, transform=None):
@@ -30,10 +43,11 @@ class spectraDataset(Dataset):
 
         spec_name = os.path.join(self.root_dir, self.parameters.iloc[idx, 0])
         
-        spectra = np.loadtxt(spec_name)
-        inputs = np.array(self.parameters.iloc[idx, 1:], dtype=np.float32)
+        spectra = np.loadtxt(spec_name, dtype=np.float32)
+        inputs = np.array(self.parameters.iloc[idx, 1:-1], dtype=np.float32)
+        rtop = np.array(self.parameters.iloc[idx, -1], dtype=np.float32)
 
-        sample = {'spectra': spectra, 'parameters': inputs}
+        sample = {'spectra': spectra, 'parameters': inputs, 'rtop': rtop}
 
         if self.transform:
             sample = self.transform(sample)

HERMES/models/model.py
@@ -17,14 +17,14 @@ class SpecGen(nn.Module):
             out_channels (int): Number of output channels.
         """
         super(SpecGen, self).__init__()
-        self.fc1 = nn.Linear(in_channels, 64) # (13 --> 64) --> (bsz, 1, 64)
+        self.fc1 = nn.Linear(in_channels, 64) # (13 --> 64) --> (1, 63, bsz)
         self.conv1 = convBlock(1, 63) # Because we torch.cat the input and output of the convBlock, we need to add the input channels to the output channels (bsz, 64, ??)
         self.conv2 = convBlock(64, 64)
         self.conv3 = convBlock(128, 128)
-        self.conv4 = convBlock(256, 256)
-        self.conv5 = convBlock(512, 512, final_layer=True) # 1024 * 64 = 64194
+        # self.conv4 = convBlock(256, 256, final_layer=True) # 512 * 64 = 32768
+        # self.conv5 = convBlock(512, 512, final_layer=True) # 1024 * 64 = 64194
         # Flattening
-        self.fc2 = nn.Linear(1024 * 64, out_channels) # (flatenned size --> out_size)
+        self.fc2 = nn.Linear(256 * 64, out_channels) # (flatenned size --> out_size)
 
     def forward(self, x: Tensor) -> Tensor:
         """
@@ -34,17 +34,18 @@ class SpecGen(nn.Module):
             x (Tensor): Input tensor.
         """
         x = F.relu(self.fc1(x))
+        x = x.unsqueeze(1)
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
+        # x = self.conv4(x)
+        # x = self.conv5(x)
         #Flatten
         x = x.view(x.size(0), -1)
         x = self.fc2(x)
         return x
 
-def L_phys_delta(model, inputs, gamma):
+def L_phys_delta(model, inputs, rtop, gamma, norm_factor):
     """
     Physical loss term that constrains the transit depth to be between (R_p/R_s)**2 and (R_p+H/R_s)**2
     where H is the height of the atmosphere model. We define R_top = R_p + H.
@@ -55,14 +56,20 @@ def L_phys_delta(model, inputs, gamma):
         gamma (float): Weighting factor for the loss term.
     """
     inputs = inputs.clone().detach().requires_grad_(True)  # enable gradient tracking
+    R_top = rtop.clone().detach().requires_grad_(True)  # enable gradient tracking
     output = model(inputs)  # shape: (batch_size, n_wavelengths)
 
     # Compute R_top/R_s and R_p/R_s
-    R_top = inputs[:, -1] # Already in meters
     R_s = inputs[:, 1] * 6.957e8 # Convert R_s from solar radii to meters
     R_p = inputs[:, 4] * 6.3781e6 # Convert R_p from Earth radii to meters
+    RP_RS = (R_p/R_s)**2 / norm_factor
+    # Match shape to output
+    RP_RS = RP_RS.unsqueeze(1).expand(-1, output.shape[1])  # shape: (batch_size, n_wavelengths)
+    RT_RS = (R_top/R_s)**2 / norm_factor
+    # Match shape to output
+    RT_RS = RT_RS.unsqueeze(1).expand(-1, output.shape[1])  # shape: (batch_size, n_wavelengths)
 
-    loss = gamma * torch.sum(F.relu(outputs - (R_top/R_s)**2) + F.relu((R_p/R_s)**2 - outputs), dim=1)
+    loss = gamma * torch.sum(F.relu(output-RT_RS) + F.relu(RP_RS - output), dim=1)
     return loss.mean()  # mean over batch
 
 O2 = [[1.26,1.28], [0.76,0.77], [0.685, 0.695]]
@@ -111,6 +118,6 @@ def L_phys_chem(model, inputs, gamma, bandpass_masks = bandpass_masks, molecule_
 
         ddelta_dxi = grad[:, idx]  # shape: (batch_size,)
         penalty = F.relu(-ddelta_dxi)  # penalize negative gradients
-        loss += penalty.mean()  # mean over batch
+        loss = loss + penalty.mean()  # mean over batch
 
     return gamma * loss

HERMES/models/train_ddp.py
@@ -4,13 +4,16 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from dataset import *
 from model import *
 import os
+from torchvision import transforms
 
+import matplotlib.pyplot as plt
+plt.style.use('/glade/u/home/aidenz/Documents/repos/EAS6995/az-paper-twocol.mplstyle')
 import torch.multiprocessing as mp
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
 from torch.utils.tensorboard import SummaryWriter
-
+torch.autograd.set_detect_anomaly(True)
 def ddp_setup():
     torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
     dist.init_process_group(backend="nccl")
@@ -36,13 +39,27 @@ class Trainer:
         self.snapshot_path = snapshot_path
         if self.gpu_id == 0:
             self.logger = SummaryWriter(tensorboard_path)
+            self.wl = np.loadtxt('/glade/derecho/scratch/aidenz/data/HERMES_labels/wl_grid.txt')
         else:
             self.logger = None
+            self.wl = None
         if os.path.exists(snapshot_path):
             print("Loading snapshot")
             self._load_snapshot(snapshot_path)
 
+        self.spec_out_path = os.path.join(os.environ['scratch_dir'], 'data/HERMES_logs/spec_out/')
+        # Check if spec_out_path exists, if not create it
+        if not os.path.exists(self.spec_out_path):
+            os.makedirs(self.spec_out_path)
+
+        self.normalization_factor = 1e-3
+
+
         self.model = DDP(self.model, device_ids=[self.gpu_id])
+
+    def _log(self, msg):
+        if self.gpu_id == 0:
+            print(msg)
 
     def _load_snapshot(self, snapshot_path):
         loc = f"cuda:{self.gpu_id}"
@@ -51,28 +68,46 @@ class Trainer:
         self.epochs_run = snapshot["EPOCHS_RUN"]
         self.optimizer.load_state_dict(snapshot["OPTIMIZER_STATE"])
         print(f"Resuming training from snapshot at Epoch {self.epochs_run}")
+        # Since epochs_run was the epoch that was completed, we want to start from the next epoch
+        self.epochs_run = self.epochs_run + 1 
 
-    def _run_batch(self, source, spectra, validate=False):
+    def _run_batch(self, source, spectra, rtop, validate=False):
+        # torch.cuda.empty_cache()
         self.optimizer.zero_grad()
-        output = self.model(source[:-1]) # Last element is the R_top, which is only for loss
-
+        output = self.model(source) # Last element is the R_top, which is only for loss
+        
         loss_MSE = F.mse_loss(output, spectra)
-        loss_delta = L_phys_delta(self.model, source, gamma=0.1)
-        loss_chem = L_phys_chem(self.model, source, gamma=0.1)
-        loss = loss_MSE + loss_delta + loss_chem
+        loss_delta = L_phys_delta(self.model, source, rtop, gamma=1e-3, norm_factor=self.normalization_factor)
+        loss_chem = L_phys_chem(self.model, source, gamma=1e4)
+        # Add loss terms and add machine precision to avoid NaN
+        loss = loss_MSE + loss_delta + loss_chem + torch.finfo(torch.float32).eps
+        if loss.isnan().any():
+            print("NaN loss detected")
+            print("Loss MSE:", loss_MSE.item())
+            print("Loss delta:", loss_delta.item())
+            print("Loss chem:", loss_chem.item())
+        
+
         loss.backward()
         self.optimizer.step()
+        # if self.gpu_id == 0:
+        #     # Print loss items
+        #     print('Batch size:', len(source),"| Loss MSE:", loss_MSE.item(), "| Loss delta:", loss_delta.item(), "| Loss chem:", loss_chem.item())
+        #     print('Total Loss:', loss.item())
+            # self._plot_spectra(self.model, epoch=1)
         return loss.item()
 
     def _run_epoch(self, epoch):
-        b_sz = len(next(iter(self.train_data))[0])
+        b_sz = len(next(iter(self.train_data))['parameters'])
         self.train_data.sampler.set_epoch(epoch)
         loss = 0
         for samples in self.train_data:
             spectra = samples['spectra'].to(self.gpu_id)
             parameters = samples['parameters'].to(self.gpu_id)
-            loss = loss + self._run_batch(parameters, spectra)
-        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)} | Loss: {loss:.4f}")
+            rtop = samples['rtop'].to(self.gpu_id)
+            loss = loss + self._run_batch(parameters, spectra, rtop)
+        train_loss = loss / len(self.train_data.dataset)
+        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)} | Loss: {train_loss:.4f}")
         
         dist.barrier()
         self.model.eval()
@@ -81,13 +116,15 @@ class Trainer:
         val_loss = torch.Tensor([val_loss]).to(self.gpu_id)
         dist.all_reduce(val_loss, op=dist.ReduceOp.SUM)
 
-        train_loss /= len(self.train_data.dataset)
         train_loss = torch.Tensor([train_loss]).to(self.gpu_id)
         dist.all_reduce(train_loss, op=dist.ReduceOp.SUM)
 
         if self.gpu_id == 0:
-            self.logger.add_scalar("Loss/val", val_loss/self.world_size, epoch)
-            self.logger.add_scalar("Loss/train", train_loss/self.world_size, epoch)
+            self.logger.add_scalar("Loss/val", val_loss/int(os.environ["WORLD_SIZE"]), epoch)
+            self.logger.add_scalar("Loss/train", train_loss/int(os.environ["WORLD_SIZE"]), epoch)
+            self.logger.flush()
+            self._save_spectra(self.model, epoch)
+            self._plot_spectra(self.model, epoch)
         self.model.train()
 
     def _validate_epoch(self):
@@ -95,17 +132,47 @@ class Trainer:
         for samples in self.val_data:
             spectra = samples['spectra'].to(self.gpu_id)
             parameters = samples['parameters'].to(self.gpu_id)
-            val_loss = val_loss + self._validate_batch(parameters, spectra)
+            rtop = samples['rtop'].to(self.gpu_id)
+            val_loss = val_loss + self._validate_batch(parameters, spectra, rtop)
+            
         return val_loss / len(self.val_data.dataset)
 
-    def _validate_batch(self, val_source, val_spectra):
+    def _validate_batch(self, val_source, val_spectra, rtop):
         with torch.no_grad():
-            output = self.model(val_source[:-1])
+            output = self.model(val_source)
             loss_MSE = F.mse_loss(output, val_spectra)
-            loss_delta = L_phys_delta(self.model, val_source, gamma=0.1)
-            loss_chem = L_phys_chem(self.model, val_source, gamma=0.1)
-            loss = loss_MSE + loss_delta + loss_chem
+            loss_delta = L_phys_delta(self.model, val_source, rtop, gamma=1e-3, norm_factor=self.normalization_factor)
+            # loss_chem = L_phys_chem(self.model, val_source, gamma=1.e4)
+            loss = loss_MSE + loss_delta # + loss_chem
             return loss.item()
+
+    def _plot_spectra(self, net, epoch):
+        if self.gpu_id != 0:
+            return
+        with torch.no_grad():
+            earth_params = torch.tensor([
+                        5772, 1, 4.4, 0, 1, 1, 255, -5.8, -1.9, -0.6, -7.6, -3.5 ,-6.5 ]).to(self.gpu_id)
+            earth_params = earth_params.unsqueeze(0)
+            output = net(earth_params)
+            output = output.cpu().numpy()
+            plt.plot(self.wl, output[0], label='Model Output')
+            plt.xlabel('Wavelength (um)')
+            plt.ylabel('Transit Depth')
+            plt.title(f"Epoch {epoch} | Model Output")
+            plt.savefig(os.path.join(self.spec_out_path, f"epoch_{epoch}.png"))
+            plt.close()
+
+    def _save_spectra(self, net, epoch):
+        if self.gpu_id != 0:
+            return
+        with torch.no_grad():
+            earth_params = torch.tensor([
+                        5772, 1, 4.4, 0, 1, 1, 255, -5.8, -1.9, -0.6, -7.6, -3.5 ,-6.5 ]).to(self.gpu_id)
+            earth_params = earth_params.unsqueeze(0)
+            output = net(earth_params)
+            output = output.cpu().numpy()
+            np.savetxt(os.path.join(self.spec_out_path, f"epoch_{epoch}.txt"), output[0], fmt='%.6e')
+
 
     def _save_snapshot(self, epoch):
         snapshot = {
@@ -118,6 +185,7 @@ class Trainer:
 
     def train(self, max_epochs: int):
         for epoch in range(self.epochs_run, max_epochs):
+            self._log(f'Starting epoch {epoch}')
             self._run_epoch(epoch)
             if self.gpu_id == 0 and epoch % self.save_every == 0:
                 self._save_snapshot(epoch)
@@ -128,11 +196,14 @@ class Trainer:
 def load_train_objs():
     data_dir = '/glade/derecho/scratch/aidenz/data/HERMES_data/'
     label_csv_path = '/glade/derecho/scratch/aidenz/data/HERMES_labels/sampled_parameters.csv'
-    dataset = spectraDataset(label_csv_path, data_dir, transform=ToTensor())
+    dataset = spectraDataset(label_csv_path, data_dir, transform=transforms.Compose([
+        ToTensor(),
+        Normalize()
+    ]))
     train_dataset, val_dataset = random_split(dataset, [0.8, 0.2], generator=torch.Generator().manual_seed(13))
     model = SpecGen(in_channels=13, out_channels=44228)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=20)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=5)
     return train_dataset, val_dataset, model, optimizer
 
 
@@ -161,8 +232,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='simple distributed training job')
     parser.add_argument('run_name', type=str, help='Name of the run')
     parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
-    parser.add_argument('--save_every', default=10, type=int, help='How often to save a snapshot')
-    parser.add_argument('--batch_size', default=64, type=int, help='Input batch size on each device (default: 32)')
+    parser.add_argument('--save_every', default=1, type=int, help='How often to save a snapshot')
+    parser.add_argument('--batch_size', default=256, type=int, help='Input batch size on each device (default: 32)')
     args = parser.parse_args()
 
     # Get scratch dir